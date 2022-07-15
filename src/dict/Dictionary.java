045ddb7655928133adfb3d2e08dae655cb07a6cc
Noah Broyles <noahbroyles@bestnest.com>
2022-07-15 13:18:37 -0400

    Update to inherit from `JSONObject`

--- src/dict/Dictionary.java
+++ src/dict/Dictionary.java
@@ -1,267 +1,198 @@
 package dict;
 
-import java.util.ArrayList;
-import java.util.regex.Matcher;
+import java.util.*;
+import org.json.JSONObject;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import org.json.JSONException;
 
 
-/***
- * This class is meant to model a dictionary structure similar to Python. I made it because I was not at all satisfied with hash tables and such. It has a very much Python-inspired toString.
+/**
+ * This class is meant to model a dictionary structure similar to Python. I made it because I was not at all satisfied with hash tables and such.
  * The only thing I wasn't able to do was figure out how to use square bracket syntax, e.g. dictionary["key"] = "value".
  * 
- * This dictionary class only supports string keys, but allows lookup of values by an integer dict position.
- * @author nbroyles
+ * This dictionary class only supports string keys, but allows lookup of values by an integer dict position. Kinda jank.
+ *
+ * @author Noah Broyles
  *
  */
-public class Dictionary {
-	
-	private ArrayList<String> keys;
-	private ArrayList<Object> values;
+public class Dictionary extends JSONObject {
 	
 	/*  Constructors  */
 	// Empty dict constructor
 	public Dictionary() {
-		this.keys = new ArrayList<String>();
-		this.values = new ArrayList<Object>();
+		super();
+	}
+
+	public Dictionary(String fromJson) {
+		super(fromJson);
+	}
+
+	public Dictionary(Map<?, ?> map) {
+		super(map);
 	}
 	
-	// Keys and values arraylist constructor
-	public Dictionary(ArrayList<String> keys, ArrayList<Object> values) {
-		this.keys = keys;
-		this.values = values;
+	public Dictionary(String[] keys, Object[] values) {
+		super(dict(keys, values));
+	}
+
+	/**
+	 * Returns a copy of a Dictionary
+	 *
+	 * @param d the dict to copy
+	 */
+	public Dictionary (Dictionary d) {
+		super(d, JSONObject.getNames(d));
 	}
 	
-	// copy constructor
-	public Dictionary(Dictionary d) {
-		this.keys = d.keys;
-		this.values = d.values;
-	}
+
 	
-	
-	
-	/* Object Methods */
-	
-	/***
+	/**
 	 * Get the value of a selector from the Dictionary. Selectors are separated by dots(.)
+	 *
 	 * @param selector the selector to lookup
 	 * @return the value the selector points to
 	 * @throws KeyError if you try anything stupid
 	 */
-	public Object get(String selector) throws KeyError {
-		return this.get(selector, "\\.");
+	public Object select(String selector) throws KeyError {
+		return this.select(selector, "\\.");
 	}
+
+	public boolean selectBoolean(String selector) throws KeyError {
+		return (boolean) this.select(selector, "\\.");
+	}
+
+	public int selectInt(String selector) throws KeyError {
+		return (int) this.select(selector, "\\.");
+	}
+
+	public double selectDouble(String selector) throws KeyError {
+		return (double) this.select(selector, "\\.");
+	}
+
+	public String selectString(String selector) throws KeyError {
+		return (String) this.select(selector, "\\.");
+	}
+
+	public BigInteger selectBigInteger(String selector) throws KeyError {
+		return new BigInteger(this.select(selector, "\\.").toString());
+	}
+
+	public BigDecimal selectBigDecimal(String selector) throws KeyError {
+		return new BigDecimal(this.select(selector, "\\.").toString());
+	}
+
+	public Dictionary selectDictionary(String selector) throws KeyError {
+		return (Dictionary) (this.select(selector, "\\."));
+	}
+
 	
-	
-	/***
-	 * Get the value of a selector from the Dictionary.
+	/**
+	 * Get the value of a selector from the Dictionary. An example of a selector would be "order.price.total_price_usd".
+	 * That selector would look for a nested Object called "total_price_usd" inside a Dictionary called "price" inside a Dictionary called "order".
+	 *
 	 * @param selector the selector to lookup
 	 * @param separator the regex to split the selector by
-	 * @return the value the selector points to
+	 * @return the (Object) value that the selector points to
 	 * @throws KeyError if you try anything stupid
 	 */
-	public Object get(String selector, String separator) throws KeyError {
-		String[] path = selector.split(separator, -1);
-		Dictionary currentDict = this;
-		
-		String currentSelector = "";
-		for (int i = 0; i < path.length; i++) {
-			String key = path[i];
-			int index = currentDict.keys.indexOf(key);
-			
-			if (index == -1) {
-				/* This path does not exist in the current dict yet, so return null.
-				 * I really don't want to throw an error here because if something doesn't exist in a dict, that's okay.
-				 * I just wanna handle it gracefully.
-				*/
-				return null;
-			} else {
-				// The path exists in the current dict
-				// If we're at the end, return the value
-				if (i == path.length -1) {
-					return currentDict.values.get(index);
-				}
+	public Object select(String selector, String separator) throws KeyError {
+		String[] keyPath = selector.split(separator);
+
+		// The goal is to return the final object at the end of the keyPath
+		try {
+			Object finalObject;
+
+			// Get all the way to the final nested JSONObject
+			JSONObject obj = new Dictionary(this);
+			for (int i = 0; i < keyPath.length - 1; i++) {
+				obj = (JSONObject) obj.get(keyPath[i]);
 			}
-			
-			// if we're not at the end of the path, bump the base
-			if (i == 0) {
-				currentSelector = key;
-			} else {
-				currentSelector = currentSelector + separator + key;
-			}
-			
-			try {
-				currentDict = (Dictionary) this.get(currentSelector, separator);
-			} catch (ClassCastException ex ) {
-				throw new KeyError(currentSelector, "not a Dictionary! Cannot get attribute '" + path[i + 1] + "'");
-			}
+			finalObject = obj.get(keyPath[keyPath.length - 1]);
+
+			return finalObject;
+		} catch (ClassCastException ex) {
+			throw new KeyError(selector, ex.getMessage());
 		}
-		
-		// If we made it to this point, the key did not exist in the dictionary.
-		return null;
+
 	}
-	
-	
-	/***
-	 * Returns the value at a certain integer index of the dictionary
-	 * @param index
-	 * @return Object
-	 */
-	public Object get(int index) {
-		return this.values.get(index);
-	}
-	
-	/***
-	 * Sets the value of a selector to the value given. If the path in the dictionary doesn't yet exist, a dictionary to hold the path will be created for you.
-	 * @param selector the path to put the object under, separated by dots(.)
-	 * @param value the object to set
-	 * @throws KeyError 
-	 */
-	public void set(String selector, Object value) throws KeyError {
-		this.set(selector, value, "\\.");
-	}
-	
-	
-	/***
-	 * Sets the value of a selector to the value given. If the path in the dictionary doesn't yet exist, a dictionary to hold the path will be created for you.
-	 * @param selector the path to put the object under
-	 * @param value the object to set
-	 * @param separator the regex to split the selector with
-	 * @throws KeyError 
+
+	/**
+	 * Set's the final path at the end of a selector to a given value. Supports nested Dictionaries, and if a Dictionary on the path
+	 * doesn't yet exist, it will be created for you.
+	 *
+	 * @param selector The selector to use
+	 * @param value The value to assign
+	 * @param separator The regex to split the selector by.
+	 * @throws KeyError If an object which is not already a dictionary would be overridden.
 	 */
 	public void set(String selector, Object value, String separator) throws KeyError {
-		String[] path = selector.split(separator, -1);
+		// Split out the path
+		String[] keyPath = selector.split(separator);
+
+		// This will change as we walk through the path
 		Dictionary currentDict = this;
-		
-		String currentSelector = "";
-		for (int i = 0; i < path.length; i++) {
-			String key = path[i];
-			int index = currentDict.keys.indexOf(key);
-			
-			if (index == -1) {
-				// This path does not exist in the current dict yet, so create it
-				currentDict.keys.add(key);
-				// If we're at the end of the path, add the value, otherwise add a new empty dictionary
-				if (i == path.length -1) {
-					currentDict.values.add(value);
-					break;
-				} else {
-					currentDict.values.add(new Dictionary());
-				}
-			} else {
-				// The path exists in the current dict
-				// If we're at the end, add the value
-				if (i == path.length -1) {
-					currentDict.values.set(index, value);
-					break;
-				}
-			}
-			
-			// if we're not at the end of the path, bump the currentDict up
-			if (i == 0) {
-				currentSelector = key;
-			} else {
-				currentSelector = currentSelector + separator + key;
-			}
-			
+		// The goal is to set the final object at the end of the keyPath
+		for (int i = 0; i < keyPath.length - 1; i++) {
+			// Let's see how far into the dict the path exists
 			try {
-				currentDict = (Dictionary) this.get(currentSelector, separator);
-			} catch (ClassCastException ex ) {
-				throw new KeyError(currentSelector, "not a Dictionary! Cannot set attribute '" + path[i + 1] + "'");
+				// Reassign the currentDict to the value of the current key
+				currentDict = (Dictionary) currentDict.get(keyPath[i]);
+			} catch (JSONException ex) {
+				// This path does not yet exist, add it
+				currentDict.put(keyPath[i], new Dictionary());
+				// Reassign the current dict to the new dictionary, so we can progress along the path
+				currentDict = (Dictionary) currentDict.get(keyPath[i]);
+			} catch (ClassCastException ex) {
+				/* This means that the path already does exist, but it ends here, in which case you would be
+				 * overriding the current value. This is not good. I think we will throw a KeyError. */
+				throw new KeyError(keyPath[i], "Value would be overridden with nested Dictionary");
 			}
 		}
+
+		// Finally, put the final value in its place.
+		currentDict.put(keyPath[keyPath.length - 1], value);
 	}
-	
-	/***
-	 * Pops an item from the dictionary and returns the value
-	 * @param selector
-	 * @return the value that was at the selector
-	 * @throws KeyError if the selector does not exist
+
+
+	/**
+	 * Create a map from two equal length arrays of keys and values
+	 *
+	 * @param keys a String[] of keys
+	 * @param values an Object[] of equal length to keys
+	 * @return a Map with matching key-value pairs
 	 */
-	public Object pop(String selector) throws KeyError {
-		String[] path = selector.split("\\.", -1);
-		Dictionary currentDict = this;
-		
-		for (int i = 0; i < path.length; i++) {
-			String key = path[i];
-			int index = currentDict.keys.indexOf(key);
-			
-			if (index == -1) {
-				// This index path does not exist
-				throw new KeyError(selector);
-			} else {
-				// The path exists in the current dict
-				// If we're at the end, remove the value
-				if (i == path.length -1) {
-					try {
-						currentDict.keys.remove(index);
-						Object val = currentDict.values.get(index);
-						currentDict.values.remove(index);
-						return val;
-					} catch (java.lang.IndexOutOfBoundsException ex) {
-						throw new KeyError(selector);
-					}
-				}
-			}
-			// if we're not at the end of the path, bump the base
-			currentDict = (Dictionary) this.get(key);
+	private static Map<String, Object> dict(String[] keys, Object[] values) {
+		assert keys.length == values.length;
+
+		int c = 0;
+		Map<String, Object> map = new HashMap<>();
+		for (String key: keys) {
+			map.put(key, values[c]);
+			c++; // hehe
 		}
-		
-		return null;
+
+		return map;
 	}
-	
-	
-	/***
-	 * Removes the selector and value at a given selector from the dictionary.
-	 * @param selector the selector to remove
-	 * @throws KeyError if there is no such selector
+
+
+	/**
+	 * Returns a JSON string representation of the object
+	 *
+	 * @param indent The amount of space to indent to prettify the output
+	 * @return a JSON string
 	 */
-	public void remove(String selector) throws KeyError {
-		this.pop(selector);
+	public String dumps(int indent) {
+		return this.toString(indent);
 	}
-	
-	
-	public int length() {
-		return this.keys.size();
-	}
-	
-	public ArrayList<String> keys() {
-		return this.keys;
-	}
-	
-	public ArrayList<Object> values() {
-		return this.values;
-	}
-	
-	
-	public String toJSON() {
-		String json = "{";
-		for (int i = 0; i < this.keys.size(); i++) {
-			json += "\"" + this.keys.get(i) + "\"" + ": ";
-			
-			// Account for NULL objects
-			if (this.values.get(i) == null) {
-				json += "null";
-			} else {
-				if (this.values.get(i).getClass().getName() == "java.lang.String") {
-					json += "\"" + new String((String) this.values.get(i)).replaceAll("\"", Matcher.quoteReplacement("\\\\\"")) + "\"";
-				} else {
-					json += this.values.get(i).toString();
-				}
-			}
-			
-			// Check to see if we should put a comma, basically if there is one after this.
-			if (i < this.keys.size() - 1) {
-				json += ", ";
-			}
-		}
-		json += "}";
-		return json;
-	}
-	
-	/***
-	 * Returns a string representation of the dictionary
+
+	/**
+	 * Returns a JSON string representation of the object
+	 *
+	 * @return a JSON string
 	 */
-	public String toString() {
-		return this.toJSON();
+	public String dumps() {
+		return this.toString();
 	}
 
 }
